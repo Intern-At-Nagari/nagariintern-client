996b708a38f6bca2194af61ebebe6422e0d26774
nabil260904 <116415990+nabil260904@users.noreply.github.com>
2025-02-11 13:13:08 +0700

    setup to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 5001,
+    host: '0.0.0.0',
+    cors: true,
+    https: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        }
+      }
+    }
+  },
+  base: '/'
 })
